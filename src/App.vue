628139f67d702d3655e640076146ae623a046076
Artem Solovev <85392692+TemaTerbi@users.noreply.github.com>
2022-01-31 12:16:44 +0300

    Create VUE SPA application

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <navbar></navbar>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar";
+export default {
+  components: {Navbar}
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+}
+</style>
